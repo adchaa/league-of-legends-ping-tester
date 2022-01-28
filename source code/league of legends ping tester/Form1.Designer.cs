a7db88105b7bde651023d81c0a955ea628862f5e
adchaa <adem0946chaa@gmail.com>
2022-01-28 18:40:27 +0100

    source code

---
+++ source code/league of legends ping tester/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace league_of_legends_ping_tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.euw_radiobutton = new System.Windows.Forms.RadioButton();
+            this.na_radiobutton = new System.Windows.Forms.RadioButton();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart1 = new LiveCharts.Wpf.CartesianChart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.Label();
+            this.now = new System.Windows.Forms.Label();
+            this.textBox_min = new System.Windows.Forms.TextBox();
+            this.textBox_max = new System.Windows.Forms.TextBox();
+            this.textBox_now = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // euw_radiobutton
+            // 
+            this.euw_radiobutton.AutoSize = true;
+            this.euw_radiobutton.Location = new System.Drawing.Point(13, 49);
+            this.euw_radiobutton.Margin = new System.Windows.Forms.Padding(4);
+            this.euw_radiobutton.Name = "euw_radiobutton";
+            this.euw_radiobutton.Size = new System.Drawing.Size(61, 21);
+            this.euw_radiobutton.TabIndex = 0;
+            this.euw_radiobutton.TabStop = true;
+            this.euw_radiobutton.Text = "EUW";
+            this.euw_radiobutton.UseVisualStyleBackColor = true;
+            // 
+            // na_radiobutton
+            // 
+            this.na_radiobutton.AutoSize = true;
+            this.na_radiobutton.Location = new System.Drawing.Point(135, 49);
+            this.na_radiobutton.Margin = new System.Windows.Forms.Padding(4);
+            this.na_radiobutton.Name = "na_radiobutton";
+            this.na_radiobutton.Size = new System.Drawing.Size(48, 21);
+            this.na_radiobutton.TabIndex = 1;
+            this.na_radiobutton.TabStop = true;
+            this.na_radiobutton.Text = "NA";
+            this.na_radiobutton.UseVisualStyleBackColor = true;
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(13, 94);
+            this.startbutton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 27);
+            this.startbutton.TabIndex = 3;
+            this.startbutton.Text = "start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(13, 174);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(794, 292);
+            this.elementHost1.TabIndex = 4;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.cartesianChart1;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(135, 94);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Location = new System.Drawing.Point(382, 103);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(36, 17);
+            this.min.TabIndex = 6;
+            this.min.Text = "MIN:";
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Location = new System.Drawing.Point(491, 103);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(41, 17);
+            this.max.TabIndex = 8;
+            this.max.Text = "MAX:";
+            // 
+            // now
+            // 
+            this.now.AutoSize = true;
+            this.now.Location = new System.Drawing.Point(613, 104);
+            this.now.Name = "now";
+            this.now.Size = new System.Drawing.Size(46, 17);
+            this.now.TabIndex = 9;
+            this.now.Text = "NOW:";
+            // 
+            // textBox_min
+            // 
+            this.textBox_min.Location = new System.Drawing.Point(424, 100);
+            this.textBox_min.Name = "textBox_min";
+            this.textBox_min.ReadOnly = true;
+            this.textBox_min.Size = new System.Drawing.Size(61, 22);
+            this.textBox_min.TabIndex = 10;
+            this.textBox_min.Text = "10000";
+            // 
+            // textBox_max
+            // 
+            this.textBox_max.Location = new System.Drawing.Point(537, 100);
+            this.textBox_max.Name = "textBox_max";
+            this.textBox_max.ReadOnly = true;
+            this.textBox_max.Size = new System.Drawing.Size(70, 22);
+            this.textBox_max.TabIndex = 11;
+            this.textBox_max.Text = "0";
+            // 
+            // textBox_now
+            // 
+            this.textBox_now.Location = new System.Drawing.Point(665, 101);
+            this.textBox_now.Name = "textBox_now";
+            this.textBox_now.ReadOnly = true;
+            this.textBox_now.Size = new System.Drawing.Size(70, 22);
+            this.textBox_now.TabIndex = 12;
+            this.textBox_now.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(485, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "min,now and max on vacation";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(241, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 27);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 492);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_now);
+            this.Controls.Add(this.textBox_max);
+            this.Controls.Add(this.textBox_min);
+            this.Controls.Add(this.now);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.na_radiobutton);
+            this.Controls.Add(this.euw_radiobutton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "league of legends ping tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton euw_radiobutton;
+        private System.Windows.Forms.RadioButton na_radiobutton;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private LiveCharts.Wpf.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.Label now;
+        private System.Windows.Forms.TextBox textBox_min;
+        private System.Windows.Forms.TextBox textBox_max;
+        private System.Windows.Forms.TextBox textBox_now;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ source code/league of legends ping tester/Form1.cs
@@ -0,0 +1,174 @@
+﻿
+using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace league_of_legends_ping_tester
+{
+
+    public partial class Form1 : Form
+    {
+        private int[] arrayofpings = new int[60];
+        private SeriesCollection ping = new SeriesCollection();
+        
+        private Thread t;
+        private bool clicked_before = false;
+        private string currentping = "0";
+        public Form1()
+        {   
+
+            
+            InitializeComponent();
+            
+
+        }
+
+
+        public void graphic_updater()
+        {
+            Ping p = new Ping();
+            PingReply reply;
+            int x;
+            int max;
+            int min;
+            while (true)
+            {
+                Thread.Sleep(993);
+                if (euw_radiobutton.Checked)
+                {
+                    reply = p.Send("riot.de");
+                }
+                else
+                {
+                    reply = p.Send("192.64.172.50");
+                }
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    x = Convert.ToInt32(reply.RoundtripTime);
+                    
+                }
+                else
+                {
+                    x = -100;
+                }
+                max = int.Parse(textBox_max.Text);
+                min = int.Parse(textBox_min.Text);
+                if (x > max)
+                {
+                    textBox_max.Invoke((MethodInvoker)delegate
+                    {
+                        textBox_max.Text = x.ToString();
+                    });
+
+                }
+               
+                if (x < min)
+                {
+                    textBox_min.Invoke((MethodInvoker)delegate
+                    {
+                        textBox_min.Text = x.ToString();
+                    });
+
+                }
+                textBox_now.Invoke((MethodInvoker)delegate
+                {
+                    textBox_now.Text = x.ToString();
+                });
+                ping[0].Values.Add(x);
+                ping[0].Values.RemoveAt(0);
+
+            }
+        }
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            startbutton.Enabled = false;
+            button3.Enabled = false;
+            euw_radiobutton.Enabled = false;
+            na_radiobutton.Enabled = false;
+            if (clicked_before)
+            {
+                t.Resume();
+            }
+            else
+            {
+                clicked_before = true;
+                t.Start();
+            }
+       
+        }
+
+        private void cartesianChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Thread(graphic_updater);
+            t.IsBackground = true;
+            cartesianChart1.Series.Clear();
+            cartesianChart1.AxisX.Add(new LiveCharts.Wpf.Axis()
+            {
+                Title = "seconds",
+                Labels = new[] {"60", "59" ,"58" , "57", "56", "55", "54", "53", "52", "51", "50", "49", "48", "47", "46", "45", "44", "43", "42", "41", "40", "39", "38" , "37" , "36" , "35" , "34" , "33" , "32" , "31" , "30" , "29" , "28" , "27" , "26" , "25" , "24" , "23" , "22" , "21" , "20" , "19" , "18" , "17" , "16" , "15" , "14" , "13" , "12" , "11" , "10" , "9" , "8" , "7" , "6" , "5" , "4" , "3", "2", "1" }
+            });
+            
+            
+            ping.Add(new LineSeries()
+            {
+                Title = "ping",
+                Values = new ChartValues<int>(arrayofpings)
+            });
+            cartesianChart1.Series = ping;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Suspend();
+            startbutton.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clicked_before = false;
+            euw_radiobutton.Enabled = true;
+            na_radiobutton.Enabled = true;
+            t = new Thread(graphic_updater);
+            t.IsBackground = true;
+            textBox_max.Text = "0";
+            textBox_min.Text = "10000";
+            textBox_now.Text = "0";
+            cartesianChart1.Series.Clear();
+
+            ping.Add(new LineSeries()
+            {
+                Title = "ping",
+                Values = new ChartValues<int>(arrayofpings)
+            });
+            cartesianChart1.Series = ping;
+        }
+
+   
+
+       
+    }
+}
